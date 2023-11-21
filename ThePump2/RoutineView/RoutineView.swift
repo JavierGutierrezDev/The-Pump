2e6ea1b411a5bd54da280cf31f5860708ac84e67
JavierGutierrezDev <gutierrezgutierrezjavier30@gmail.com>
2023-11-21 01:25:20 +0100

    implemented feature edit exercise

--- ThePump2/RoutineView/RoutineView.swift
+++ ThePump2/RoutineView/RoutineView.swift
@@ -11,47 +11,20 @@ struct RoutineView: View {
     @Environment (\.modelContext) var context
     @Environment (\.dismiss) var dismiss
     @State var routine : Routine
-
-    //@Query private var exercises : [Exercise]
-    //private var exercises : [Exercise]
     
     @State private var isCreateExerciseView = false
     
-    //@State var isRoutineFinished : Bool = false
-
-//    init( routine: Routine) {
-//        
-//        self.routine = routine
-//        //self.exercises = routine.exercises
-//        
-//    }
-
+  
     var body: some View {
         VStack{
             
             List{
 
                 ForEach($routine.exercises.sorted(by: { $0.wrappedValue.creationDate < $1.wrappedValue.creationDate }), id: \.id) { exercise in
-                    ExerciseView(exercise: exercise)
-                    .swipeActions{
-                        Button("Delete", systemImage: "trash.fill", role: .destructive) {
-//                                //elimina el ejercicio de Core Data
-//                                context.delete(exercise)
-                                    
-                                // Eliminar el ejercicio del modelo de datos de SwiftUI
-                                routine.exercises.remove(at: routine.exercises.firstIndex(of: exercise.wrappedValue)!)
-                            
-                                // Guardar los cambios en el contexto de Core Data
-                                try? context.save()
-                        }
-                        
-                        Button("Edit", systemImage: "pencil", role: .none) {
-                            print("Edit -->\(exercise.exerciseName.wrappedValue)")
-                        }
-                        .foregroundStyle(.primary)
-                    }
+                    ExerciseView(exercise: exercise, routine: $routine)
                     
                 }
+                
                 
             }
             .listStyle(.plain)
