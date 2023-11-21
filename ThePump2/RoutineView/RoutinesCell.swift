9712aa3ccbdd7593df3318adc95e6337611466c9
JavierGutierrezDev <gutierrezgutierrezjavier30@gmail.com>
2023-11-21 01:30:57 +0100

    edit routine feature implemented

--- ThePump2/RoutineView/RoutinesCell.swift
+++ ThePump2/RoutineView/RoutinesCell.swift
@@ -8,21 +8,42 @@
 import SwiftUI
 
 struct RoutinesCell : View {
-    @State private var navigateToNextView = false
+    @Environment (\.modelContext) var context
+    @State private var navigateToNextView : Bool = false
+    @State private var editRoutine : Bool = false
     var routine: Routine
     var body: some View {
         
         NavigationStack{
-            VStack(alignment: .leading){
-                
-                Text(routine.name)
-                    .padding(.top)
-                    .font(.title3)
-                    .bold()
-                
-                Text(routine.routineDescription)
-                    .font(.footnote)
-                    .padding(.vertical , 0.5)
+            
+            VStack{
+                HStack(alignment: .top){
+                    VStack(alignment: .leading){
+                        Text(routine.name)
+                            .padding(.top)
+                            .font(.title3)
+                            .bold()
+                        
+                        Text(routine.routineDescription)
+                            .font(.footnote)
+                            .padding(.vertical , 0.5)
+                    }
+                    Spacer()
+                    Menu("", systemImage: "ellipsis") {
+                        Button("Edit", systemImage: "pencil") {
+                            editRoutine.toggle()
+                        }
+                        
+
+                        Button("Delete", systemImage: "trash",role: .destructive) {
+                            context.delete(routine)
+                        }
+                    }
+                    .foregroundStyle(.foreground)
+                    .sheet(isPresented: $editRoutine, content: {
+                        AddRoutineView(routine: routine)
+                    })
+                }
                 HStack{
                     Button(action: {
                         navigateToNextView.toggle() // Cambia el estado para activar la navegación
