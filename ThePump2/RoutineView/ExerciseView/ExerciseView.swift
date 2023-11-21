663f3ebf3067fd619336cd59bebfea99c2f49da8
JavierGutierrezDev <gutierrezgutierrezjavier30@gmail.com>
2023-11-21 01:29:30 +0100

    description bug solved

--- ThePump2/RoutineView/ExerciseView/ExerciseView.swift
+++ ThePump2/RoutineView/ExerciseView/ExerciseView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ExerciseView: View {
     @Binding var exercise : Exercise
-    
+    @Binding var routine : Routine
     @Environment (\.modelContext) var context
     @State var anotationText : String = ""
     var body: some View {
@@ -25,48 +25,72 @@ struct ExerciseView: View {
                     Text(exercise.exerciseName)
                         .font(.title3)
                     TextField(text: $anotationText) {
-                        Text("Agregar descripción")
+                        Text((exercise.anotation != "") ? exercise.anotation :  "Add description")
                             .font(.subheadline)
                             .foregroundStyle(.gray)
+                    }.onSubmit {
+                        exercise.anotation = anotationText
                     }
+                    
                     
                     
                 }
                 
-                Button(action: {
-                    print(exercise.exerciseName)
+                //Menu de opciones relacionadas con el ejercicio
+                Menu("", systemImage: "ellipsis") {
+                    Button("Reorder", systemImage: "arrow.up.arrow.down") {
+                        
+                    }
+                    Button("Replace", systemImage:"arrow.triangle.2.circlepath") {
+                        
+                    }
+                    Button("Add to superset", systemImage:"plus") {
+                        
+                    }
+                    Button("Delete", systemImage:"xmark", role: .destructive) {
+                        // Eliminar el ejercicio del modelo de datos de SwiftUI
+                        routine.exercises.remove(at: routine.exercises.firstIndex(of: exercise)!)
                     
-                                    }, label: {
-                    Image(systemName: "ellipsis")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 20,height: 20)
-                })
+                        // Guardar los cambios en el contexto de Core Data
+                        
+                        try? context.save()
+                    }
+                }
+                .foregroundStyle(.foreground)
+                .padding(.horizontal)
+
                 Spacer()
             }
             .frame(width: .infinity)
             .padding()
-            HStack{
-                Button(action: {}, label: {
-                    Image(systemName: "alarm")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 20)
-                    
-                    Text("Temporizador de descanso (desactivado)")                })
+            
+            //Función del cronometro
+            Button("", systemImage: "alarm") {
                 
             }
+            .foregroundStyle(.foreground)
+            
             ScrollView{
-                
+                	
                 ForEach($exercise.sets.sorted { $0.wrappedValue.creationDate < $1.wrappedValue.creationDate }, id: \.wrappedValue.id) { set in
                     SetsCell(set: set)
                         
                 }
+                HStack(){
+                    
+                    Button("New Set", systemImage: "plus") {
+                        exercise.addSet(reps: 0, weights: 0, setType: .normal)
+                    }
+                    .padding(.horizontal)
+                    Button("Delete Set", systemImage: "trash", role: .destructive) {
+                        
+                        exercise.deleteSet()
+                    }
+                    .padding(.horizontal)
 
-                Button("New Set", systemImage: "plus") {
-                    exercise.addSet(reps: 0, weights: 0, setType: .normal)
                 }
             }
+            
             
         }
     }
