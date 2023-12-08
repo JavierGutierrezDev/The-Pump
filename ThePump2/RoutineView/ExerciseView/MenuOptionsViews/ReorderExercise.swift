b6b6d1d1e2e6182d09463a06b07d0dd687f75312
JavierGutierrezDev <gutierrezgutierrezjavier30@gmail.com>
2023-12-08 19:03:06 +0100

    primera implementacion de reordenar ejercicios

--- ThePump2/RoutineView/ExerciseView/MenuOptionsViews/ReorderExercise.swift
+++ ThePump2/RoutineView/ExerciseView/MenuOptionsViews/ReorderExercise.swift
@@ -8,11 +8,84 @@
 import SwiftUI
 
 struct ReorderExercise: View {
+    @Environment (\.modelContext) private var context
+    @Environment (\.dismiss) private var dismiss
+    @Binding  var routine : Routine
+    @State private var exercise1 : Exercise?
+    @State private var exercise2 : Exercise?
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Select the exercises")
+            .font(.title2)
+            .padding()
+        List {
+            ForEach($routine.exercises.sorted(by: { $0.wrappedValue.creationDate < $1.wrappedValue.creationDate }), id: \.id){exercise in
+                HStack{
+                    Image( exercise.imageName.wrappedValue)
+                        .resizable()
+                        .frame(width: 60, height: 60)
+                        .aspectRatio(contentMode: .fit)
+                    
+                    Text(exercise.exerciseName.wrappedValue)
+                        .font(.title3)
+                        .padding(.horizontal)
+                    
+                    Spacer()
+                    
+                    Button {
+                        if(exercise1 == nil  ){
+                            exercise1 = exercise.wrappedValue
+                        }
+                        else{
+                            if(exercise2 == nil){
+                                exercise2 = exercise.wrappedValue
+                                var exercise1Date = exercise1?.creationDate
+                                var exercise2Date = exercise2?.creationDate
+                                exercise1?.creationDate = exercise2Date!
+                                exercise2?.creationDate = exercise1Date!
+                                
+                                exercise1 = nil
+                                exercise2 = nil
+                                
+                                
+                            }
+                        }
+                    } label: {
+                        Image(systemName: "checkmark")
+                            .foregroundStyle((exercise1?.id == exercise.wrappedValue.id || exercise2?.id == exercise.wrappedValue.id ) ? .blue : .gray)
+                            
+                    }
+
+                    
+                    
+                    
+                    
+                    
+                    
+                }
+
+            }
+//            .onMove(perform: move)
+        }
+        .padding(.vertical)
+        
+        
     }
+    
+//    func move(from source: IndexSet, to destination: Int){
+//       print("Move-->\(source)")
+//       print("Destination-->\(destination)")
+//        //array de ejercicios ordenados
+//        var exercises = routine.exercises.sorted(by: {$0.creationDate < $1.creationDate})
+//        
+//        print(exercises[source.first!].exerciseName)
+//
+//    }
 }
 
-#Preview {
-    ReorderExercise()
-}
+//#Preview {
+//    ReorderExercise(
+//        routine: Routine(name: "Test",
+//                         exercises: [Exercise(imageName: "barbellSquat",exerciseName: "Barbell Squat",sets: [Set(reps: 12,weights: 22,setType: .dropset)])],
+//                         routineDescription: "testRoutineDescription"
+//                                    )
+//}
